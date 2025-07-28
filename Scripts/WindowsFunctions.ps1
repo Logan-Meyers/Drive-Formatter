3ac8d85fe53c4ae2c57a5e58997c366834c2a95d
Logan Meyers <theterrarian03@gmail.com>
2025-07-28 14:35:43 -0700

    Update Windows functions to work like mac funcs

--- Scripts/WindowsFunctions.ps1
+++ Scripts/WindowsFunctions.ps1
@@ -1,6 +1,8 @@
 ### This function lists all drives other than 0, and asks for which drive the user would like to manage
 ### For example, this might return 1
 function Get-WantedDrive($actionPrompt) {
+    Write-Host "Loading drives..."
+    
     # list of disks
     $disks = Get-Disk | Where-Object { $_.Number -ne 0 }
     
@@ -17,16 +19,17 @@ function Get-WantedDrive($actionPrompt) {
     while ($true) {
         Clear-Host
 
-        Write-Host "Use arrow keys to change which drive you'd like to $actionPrompt"
-        Write-Host "Press enter to select the drive and continue"
-        Write-Host "Press escape to go back to the main menu"
+        Write-Host "ARROW KEYS: change which drive you'd like to $actionPrompt"
+        Write-Host "ENTER: select the drive and continue"
+        Write-Host "ESCAPE: return back to the main menu"
         Write-Host ""
 
         $cur = 0
         foreach ($disk in $disks) {
             $cur += 1
 
-            Write-Host "$(if ($cur -eq $selected) { '> ' } else { '  ' })Drive $($disk.Number) | $($disk.FriendlyName) | $([math]::round($disk.Size / 1GB, 2)) GB)"
+            $shortDesc = "Drive $($disk.Number) | $($disk.FriendlyName): $([math]::round($disk.Size / 1GB, 2)) GB"
+            Write-Host "$(if ($cur -eq $selected) { '> ' } else { '  ' })$shortDesc"
         }
 
         Write-Host "$(if ($selected -eq $maxSelection) { '> ' } else { '  ' })Re-scan for drives"
@@ -80,31 +83,53 @@ function Show-CheckMenu {
     # Get the first partition of the chosen drive
     $firstPartition = Get-Partition -DiskNumber $chosenDrive | Select-Object -First 1
 
-    # Check if the partition exists
-    if ($firstPartition) {
-        # Get the partition type and offset
-        $partitionType = $firstPartition.Type  # For GPT partitions
-        $partitionOffset = $firstPartition.Offset  # Offset in bytes
+    Write-Host $firstPartition
 
-        # Check for FAT type (0E) and offset of 1024
-        if ($partitionType -eq 'XINT13' -and $partitionOffset -eq 1048576) {
-            Write-Host "Formatted correctly!"
-        } else {
-            Write-Host "Not formatted correctly!"
-            Write-Host "You'll need to format this drive from the main menu."
-            Write-Host ""
+    # Exit if no partitions on the drive (no first partition)
+    if (-not $firstPartition) {
+        Write-Host "The USB Drive has no partitions! You'll need to format it."
+        Return 0
+    }
 
-            # debugging / additional info
-            if ($partitionType -ne "XINT13") {Write-Host "- Wrong partition type: $partitionType"}
-            if ($partitionOffset -ne 1048576) {Write-Host "- Wrong Offset: $partitionOffset"}
-        }
+    # Get the partition type and offset
+    $partitionType = $firstPartition.Type  # For GPT partitions
+    $partitionOffset = $firstPartition.Offset  # Offset in bytes
+    $partitionSize = $firstPartition.Size  # Size in bytes
+
+    $twoGB = 2 * 1024 * 1024 * 1024
+    $eightMb = 8 * 1024 * 1024
+
+    $correctlyFormatted = 1
+
+    # Partition type: Either Windows_FAT_16 or DOS_FAT_16
+    if ($partitionType -ne "XINT13" -and $partitionType -ne "Logical") {
+        $correctlyFormatted = 0
+        Write-Host "!! The partition has the wrong partition type! ($($firstPartInfo["Partition Type"]))"
+    }
+
+    # Offset of 1024
+    if ($partitionOffset -ne 1048576) {
+        $correctlyFormatted = 0
+        Write-Host "!! The partition has the wrong partition offset! ($partitionOffsetBytes)"
+    }
+
+    # Size < 2GB and > 8MB
+    if ($partitionSize -gt $twoGB -or $partitionSize -lt $eightMB) {
+        $correctlyFormatted = 0
+        Write-Host "!! The partition has the wrong size! ($diskSizeBytes)"
+    }
+
+    # Final results
+    if ($correctlyFormatted -gt 0) {
+        Write-Host "-- The partition is correctly formatted! --"
     } else {
-        Write-Host "No partitions found on the selected drive."
+        Write-Host ""
+        Write-Host ">> This drive will need to be formatted! <<"
     }
 
     Write-Host ""
     Write-Host "Press enter to continue back to main menu..."
-    Read-Colonless
+    Read-Host
     Clear-Host
 }
 
@@ -147,26 +172,30 @@ function Show-FormatMenu {
 
     Clear-Host
 
-    Write-Host "Please enter a name, for what you'd like to call this drive"
-    Write-Host "(Less than or equal to 8 characters)"
-    Write-Host "(Will automatically be converted to all caps)"
-    Write-Host ""
-    Write-Host "> " -NoNewline
-    $name = Read-Colonless
+    $name = Get-FatPartName
 
     # 1. Clear disk
+    Write-Host "Clearing disk..."
     Clear-Disk -Number $chosenDrive -RemoveData -Confirm:$false
 
     # 2. Initialize with MBR
+    Write-Host "Setting to MBR format..."
     Set-Disk -Number $chosenDrive -PartitionStyle MBR
 
-    # 3. Create a new partition with whole disk
-    $newPartition = New-Partition -DiskNumber $chosenDrive -Size 2GB -Offset 1MB -AssignDriveLetter
+    # 3. Create a new partition with the calculated size
+    Write-Host "Creating partition..."
+    $usableSizeBytes = $disk.Size - 1MB
+    $maxSizeBytes = 2GB
+    $partitionSizeBytes = if ($usableSizeBytes -lt $maxSizeBytes) { $usableSizeBytes } else { $maxSizeBytes }
+
+    $newPartition = New-Partition -DiskNumber $chosenDrive -Size $partitionSizeBytes -Offset 1MB -AssignDriveLetter
 
     # 4. Format partition - TEMP DEBUGGING
+    Write-Host "Formatting partition..."
     Format-Volume -DriveLetter $newPartition.DriveLetter -FileSystem FAT -NewFileSystemLabel $name -Confirm:$false
 
     # 5. Open in explorer
+    Write-Host "Opening in Explorer..."
     Start-Process "explorer.exe" "$($newPartition.DriveLetter):\"
 
     Clear-Host
@@ -176,5 +205,40 @@ function Show-FormatMenu {
 ### This function handles renaming a partition
 ### This will ask for a drive, a new partition name, and renames the 1st partition
 function Show-RenameMenu {
-    Write-Host "Windows not supported for this function!"
+    $chosenDriveNumber = Get-WantedDrive "rename"
+
+    if (-not $chosenDriveNumber) {
+        Clear-Host
+        return
+    }
+
+    Clear-Host
+
+    # Get the first partition on the chosen disk (lowest partition number)
+    $firstPartition = Get-Partition -DiskNumber $chosenDriveNumber | Sort-Object PartitionNumber | Select-Object -First 1
+
+    if (-not $firstPartition) {
+        Write-Host "Sorry, there are no partitions on this disk! You'll need to format it first."
+        return
+    }
+
+    # Get the volume associated with this partition
+    $volume = Get-Volume -Partition $firstPartition
+
+    if (-not $volume) {
+        Write-Host "No volume found on the first partition. Cannot rename."
+        return
+    }
+
+    $oldName = $volume.FileSystemLabel
+
+    $newName = Get-FatPartName -partition $chosenDrive -oldName $oldName
+
+    # Rename the volume
+    try {
+        Set-Volume -DriveLetter $volume.DriveLetter -NewFileSystemLabel $newName -ErrorAction Stop
+        Write-Host "Successfully renamed volume from '$oldName' to '$newName'."
+    } catch {
+        Write-Host "Failed to rename volume: $_"
+    }
 }
